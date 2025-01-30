6fe4c05ae06a8c6779c3049a9ba5512909c8563c
kanokpich18 <s6603052421186@email.kmutnb.ac.th>
2025-01-30 12:28:05 +0700

    Create content2 And header2 Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { userContext } from "./context";
 import Content from "./context-content";
-
-export default function App(){
-  return(
-    <userContextProvider value={'Tom Jerry'}>
-      <Content/>
-    </userContextProvider>
-  )
+import Header2 from "./context-header2";
+import Content2 from "./context-content2";
+ 
+function App() {
+  
+  let [user,setUser] = React.useState()
+  return (
+  
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
+    </userContext.Provider>
+    )
+  
 }
+ 
+export default App;
